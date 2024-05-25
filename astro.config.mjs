a41f43a35cafc5a41b7fadfeba6139ebeea7da90
János Illés <ijanos@gmail.com>
2024-05-25 12:16:32 +0200

    move poster downloading into an Astro plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import posterIntegration from './src/integrations/poster-downloader';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [
+    tailwind(),
+    posterIntegration(),
+  ],
 });
---
+++ src/integrations/poster-downloader.ts
@@ -0,0 +1,46 @@
+import type { AstroIntegration } from "astro";
+
+import { writeFile } from 'node:fs/promises';
+import { existsSync } from 'node:fs';
+
+import omdbkey from "../../res/omdbkey.js";
+import watchlist from "../../src/data/watchlist.json";
+import { type Movie } from "../data/movies.ts";
+
+
+async function downloadPoster(imdbid: string, path: string) {
+  const response = await fetch(`https://www.omdbapi.com/?i=${imdbid}&apikey=${omdbkey}`);
+  const movie = await response.json();
+  const posterURL = movie["Poster"];
+
+  if (!posterURL || posterURL == "N/A") { return false; }
+
+  console.log("Downloading poster for ", imdbid);
+  const posterResposne = await fetch(posterURL);
+  const buffer = Buffer.from(await posterResposne.arrayBuffer());
+  await writeFile(path, buffer, { flush: true });
+  return true;
+}
+
+function checkPosters(watchlist: Array<Movie>) {
+  watchlist.forEach(async movie =>  {
+    const posterImagePath = `posters/${movie.imdbID}.jpg`;
+    if (!existsSync(posterImagePath)) {
+      if(!await downloadPoster(movie.imdbID, posterImagePath)) {
+          throw new Error(`Couldn't download poster for ${movie.imdbID}`);
+      }
+    }
+  });
+}
+
+
+export default function posterIntegration(): AstroIntegration {
+  return {
+    name: "poster-downloader",
+    hooks: {
+      "astro:config:setup": (_options) => {
+        checkPosters(watchlist as Array<Movie>);
+      },
+    },
+  };
+}
