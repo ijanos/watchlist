9645292b9f8f00d93db142d236046cc9941b23bf
János Illés <ijanos@gmail.com>
2025-02-08 17:45:46 +0100

    add eslint
    force lint before build and deploy

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+import js from "@eslint/js";
+import eslintPluginAstro from 'eslint-plugin-astro';
+
+export default [
+  {
+    ignores: ['dist/**', '.astro/**']
+  },
+  js.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+];
