c92b2325b6cec334f3905de45fe299043985b35c
János Illés <ijanos@gmail.com>
2025-11-29 13:28:28 +0100

    Tweak tabulator tables, group by watch years

--- src/pages/tabulator.json.ts
+++ src/pages/tabulator.json.ts
@@ -1,16 +1,17 @@
-import watchlist from "../data/watchlist.json";
+import { movies as watchlist } from "../data/movies.ts";
 import { getImage } from "astro:assets";
 
 const movies = await Promise.all(watchlist.map(async (m) => {
   const posterImageModule: { default: ImageMetadata } = await import(`../../posters/${m.imdbID}.jpg`);
   const posterImage = posterImageModule.default;
-  const optimizedImg = await getImage({src: posterImage, width: 125})
+  const optimizedImg = await getImage({src: posterImage, width: 65})
 
   return {
     poster: optimizedImg.src,
     title: m.englishTitle,
     director: m.director,
-    release: m.releaseYear
+    release: m.releaseYear,
+    watchyear: m.watched ? m.watched.slice(0,4) : m.releaseYear,
   }
 }));
 
