03c5b94aca9830f95833e269f310bd0deb82c271
János Illés <ijanos@gmail.com>
2026-05-09 10:22:06 +0200

    npm update
    and switch to tailwind/postcss because the vite plugin was conflicting
    between vite 7 and vite 8 during builds.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
 
 import posterIntegration from './src/integrations/poster-downloader';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [posterIntegration()],
-  vite: {
-    plugins: [tailwindcss()],
-  },
 });
