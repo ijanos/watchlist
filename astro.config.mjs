ba60cad9026f022379350478e2dd659267b23c93
János Illés <ijanos@gmail.com>
2026-07-05 18:33:38 +0200

    Update Astro.js to v7
    Remove postcss.config.mjs and add @tailwindcss/vite to fix
    tailwind and postcss issues
    
    see https://civiks.tech/writing/astro-7-upgrade/

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 import posterIntegration from './src/integrations/poster-downloader';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [posterIntegration()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
